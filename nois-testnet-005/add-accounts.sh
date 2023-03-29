acd8091c04da1a112c25d54546c99f700536626d
farukyasar <farukyasar84@gmail.com>
2023-03-29 09:45:59 +0300

    add farukyasar nois address

--- nois-testnet-005/add-accounts.sh
+++ nois-testnet-005/add-accounts.sh
@@ -5,6 +5,7 @@ genesis_addresses=(
     # Example addresses
     "nois1pq6spkms6p42djr9tsfqmlnngaf7aht8t84anz"
     "nois1zefr9ncfpyjm3af6g7e4sxf3ywmlecyjtedtsp"
+    "nois1epgapw0pea4dz2pmhpneqf7fezkldmcvcd82sz"
     # Add your address here in a PR
 
 )
